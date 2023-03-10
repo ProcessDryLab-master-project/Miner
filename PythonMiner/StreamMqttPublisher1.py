0329eb7931b02216caba1a15e7c005161b6cd4ae
Squidman95 <squidmanse@gmail.com>
2023-03-10 13:28:09 +0100

    Stream miner with MQTT

--- PythonMiner/StreamMqttPublisher1.py
+++ PythonMiner/StreamMqttPublisher1.py
@@ -1,15 +1,21 @@
+# Mqtt medium guide: https://medium.com/python-point/mqtt-basics-with-python-examples-7c758e605d4
 import paho.mqtt.client as mqtt
-from random import randrange, uniform
+from random import randrange, uniform, choice
+import string
 import time
+
+# Variables that we could get from elsewhere
+topic = "EventStream"
+clientName = "Event Publisher"
 
 # publicly available broker. We need our own
 mqttBroker = "mqtt.eclipseprojects.io"  # Don't specify "https://"
 
-client = mqtt.Client("Temperature_Inside")
+client = mqtt.Client(clientName)
 client.connect(mqttBroker)
 
 while True:
-    randNumber = uniform(20.0, 21.0)
-    client.publish("TEMPERATURE", randNumber)
-    print("Just published " + str(randNumber) + " to topic TEMPERATURE")
+    stringToSend = choice(string.ascii_letters)
+    client.publish(topic, stringToSend)
+    print(f"Just published {stringToSend} to topic {topic}")
     time.sleep(1)
