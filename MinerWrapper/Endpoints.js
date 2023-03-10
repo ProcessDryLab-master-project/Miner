c066254deff06272e7a9ad810bf3fe5151951469
Squidman95 <squidmanse@gmail.com>
2023-03-10 14:37:06 +0100

    Fixed bug in last refactor

--- MinerWrapper/Endpoints.js
+++ MinerWrapper/Endpoints.js
@@ -20,7 +20,36 @@ export default function initEndpoints(app) {
     const json = JSON.parse(file);
     res.send(json);
   });
-  
+ 
+
+  app.post(`${endpointStart}/miner`, async function (req, res) {
+    let body = await req.body;
+    console.log("body: ", body);
+    let repositoryInputPath = body.repositoryInputPath;
+    let repositoryOutputPath = body.repositoryOutputPath;
+    let incomingFileId = body.fileId; // The id for the file we request from repo
+    let fileExtension = body.fileExtension;
+    let logName = `${incomingFileId}.${fileExtension}`
+    let resourceType = "Visualization" // TODO: Specify the type of resource to be generated. Should miner decide this? Or frontend?
+
+    const fileURL = new URL(incomingFileId, repositoryInputPath).toString();
+    console.log("\n\n\nURL to get file: " + fileURL);
+
+    let fileSavePath = `./Downloads/${logName}`;
+    let repoGetResp = await getResourceFromRepo(fileURL, fileSavePath);
+    console.log(`Log saved to ${fileSavePath}`);
+
+    let minerResult = await Wrapper(fileSavePath, incomingFileId, fileExtension);
+    console.log("Wrapper miner result: " + minerResult);
+
+    console.log("URL to send result: " + repositoryOutputPath);
+    let repoPostResp = await sendResourceToRepo(repositoryOutputPath, minerResult, incomingFileId, resourceType);
+    console.log("repoPostResp: " + repoPostResp);
+    res.send(repoPostResp);
+    // res.sendStatus(200);
+  });
+
+// with the old front end:
   app.get(`${endpointStart}/miners`, function(req, res){
     console.log("Getting a request on /api/v1/miners");
 
@@ -77,33 +106,6 @@ export default function initEndpoints(app) {
     res.send(test_res_andreas_structure);
   });
 
-  app.post(`${endpointStart}/miner`, async function (req, res) {
-    let body = await req.body;
-    console.log("body: ", body);
-    let repositoryInputPath = body.repositoryInputPath;
-    let repositoryOutputPath = body.repositoryOutputPath;
-    let incomingFileId = body.fileId; // The id for the file we request from repo
-    let fileExtension = body.fileExtension;
-    let logName = `${incomingFileId}.${fileExtension}`
-
-    const fileURL = new URL(incomingFileId, repositoryInputPath).toString();
-    console.log("\n\n\nURL to get file: " + fileURL);
-
-    let fileSavePath = `./Downloads/${logName}`;
-    let repoGetResp = await getResourceFromRepo(fileURL, fileSavePath);
-    console.log(`Log saved to ${fileSavePath}`);
-
-    let minerResult = await Wrapper(fileSavePath, incomingFileId, fileExtension);
-    console.log("Wrapper miner result: " + minerResult);
-
-    console.log("URL to send result: " + repositoryOutputPath);
-    let repoPostResp = await sendResourceToRepo(repositoryOutputPath, minerResult, incomingFileId);
-    console.log("repoPostResp: " + repoPostResp);
-    res.send(repoPostResp);
-    // res.sendStatus(200);
-  });
-
-// with how front end works now
   app.post(`${endpointStart}/miners/instance`, async function (req, res) {
     let body = await req.body;
     console.log("body: ", body);
@@ -112,7 +114,6 @@ export default function initEndpoints(app) {
     let repositoryOutputPath = new URL("api/v1/resources", body.repository);
     let logName = body.inputs["Source file"].name;
     let fileName = logName.replace(/\.[^/.]+$/, "");
-    let resourceType = "Visualization" // TODO: Specify the type of resource to be generated. Should miner decide this? Or frontend?
 
     let endpoint = path.join("api/v1/resources", fileId, "content");
     const fileURL = new URL(endpoint, repositoryInputPath).toString();
