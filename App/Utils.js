b839e3351062bbb1fc36c7bdcc1cef710bff4e42
Squidman95 <squidmanse@gmail.com>
2023-05-20 12:35:21 +0200

    Update Utils.js

--- App/Utils.js
+++ App/Utils.js
@@ -73,6 +73,7 @@ export function isObjEmpty (obj) {
 export function appendUrl(urls = []){
   let concatPath = "";
   urls.forEach(url => {
+    // TODO: if url is undefined, break and handle it.
     concatPath = path.join(concatPath, url.toString());
   });
   return new URL(concatPath);
